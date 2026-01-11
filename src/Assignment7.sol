a72377c1d1b0c88ee69728e147d9244d32fa7fd2
karima <karimareda.120@gmail.com>
2026-01-11 23:50:22 +0200

    this is assignment 7 again1

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Assignment7 is ERC20 {
+    address owner;
     // Constructor to initialize the token
     // Fill in the parameters for the token name and symbol
     constructor(string memory _name , string memory _symbol) ERC20 (_name , _symbol){
